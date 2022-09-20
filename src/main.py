2100ff4a0a0d0bd87ef5397b398a8988f4be5a70
frog <nastya.seleznyovaa@gmail.com>
2022-09-20 18:14:35 +0300

    created a server and updated requirements

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
+from flask import Flask, session
+app = Flask(__name__)
+
+app.config.update(SECRET_KEY="sdasda")
+
+@app.route('/')
+def server():
+    if 'visits' in session:
+        session['visits'] = session.get('visits') + 1
+    else:
+        session['visits'] = 1
+    return "Hellow world {}".format(session.get('visits'))
+
 if __name__ == "__main__":
-    print("NoSQL Bottle Neck Project")
+    app.run('0.0.0.0')
