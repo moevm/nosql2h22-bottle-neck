f57d96cf27f91ea37d49b194d557d9dd7100ab0d
mirrin00 <mirrin1905@outlook.com>
2022-11-20 12:07:42 +0000

    Changed plotting from matplotlib to PIL

--- src/drawing.py
+++ src/drawing.py
@@ -1,10 +1,11 @@
-import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 from io import BytesIO
 
 
-BACKGROUND_COLOR = (211 / 256, 211 / 256, 211 / 256)
-LINE_SIZE = 2.0  # Fixed or dynamic?
-PX = 1 / plt.rcParams['figure.dpi']
+BACKGROUND_COLOR = (211, 211, 211)  # Gray
+WHITE_COLOR = (255, 255, 255)
+LINE_SIZE = 7  # Fixed or dynamic?
+MARGIN = 15
 
 
 def get_road_color(road: dict):
@@ -13,21 +14,37 @@ def get_road_color(road: dict):
 
 
 def create_map_image(roads: list, size: tuple[int, int], colored=False) -> bytes:
-    fig = plt.figure(figsize=(size[0] * PX, size[1] * PX), facecolor=BACKGROUND_COLOR)
-    ax = fig.gca()
-    ax.set_facecolor(BACKGROUND_COLOR)
+    img = Image.new("RGB", (size[0] + 2 * MARGIN, size[1] + 2 *MARGIN), BACKGROUND_COLOR)
     lines = []
+    xs = []
+    ys = []
     colors = []
     for road in roads:
-        lines.append((road["start"][0], road["end"][0]))
-        lines.append((road["start"][1], road["end"][1]))
+        lines.append((road["start"], road["end"]))
+        xs.append(road["start"][0])
+        xs.append(road["end"][0])
+        ys.append(road["start"][1])
+        ys.append(road["end"][1])
         if colored:
-            colors.append(get_road_color(road))
-    if colored:
-        ax.set_prop_cycle(color=colors)
-    else:
-        ax.set_prop_cycle(color=["white"])
-    ax.plot(*lines, linewidth=LINE_SIZE)
+            colors.append(get_road_color(road["car_count"], road["capacity"]))
+    min_x, min_y = min(xs), min(ys)
+    max_x, max_y = max(xs), max(ys)
+    x_len = max_x - min_x
+    y_len = max_y - min_y
+    lines_gen = zip(lines, colors) if colored else zip(lines)
+    draw = ImageDraw.Draw(img)
+    for item in lines_gen:
+        start, end = item[0]
+        color = item[1] if colored else WHITE_COLOR
+        new_start = (
+            (start[0] - min_x) / x_len * size[0] + MARGIN,
+            size[1] - (start[1] - min_y) / y_len * size[1] + MARGIN
+        )
+        new_end = (
+            (end[0] - min_x) / x_len * size[0] + MARGIN,
+            size[1] - (end[1] - min_y) / y_len * size[1] + MARGIN
+        )
+        draw.line((new_start, new_end), fill=color, width=LINE_SIZE)
     bs = BytesIO()
-    fig.savefig(bs, format="png")
+    img.save(bs)
     return bs.getvalue()
