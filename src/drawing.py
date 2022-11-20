4ba46dbe4dd424de9afa4fd5be41b71f8aed7ac1
mirrin00 <mirrin1905@outlook.com>
2022-11-20 12:48:13 +0000

    Fixes

--- src/drawing.py
+++ src/drawing.py
@@ -4,13 +4,14 @@ from io import BytesIO
 
 BACKGROUND_COLOR = (211, 211, 211)  # Gray
 WHITE_COLOR = (255, 255, 255)
-LINE_SIZE = 7  # Fixed or dynamic?
+LINE_SIZE = 2  # Fixed or dynamic?
 MARGIN = 15
+MAX_WORKLOAD = 10
 
 
 def get_road_color(road: dict):
-    c = max(min(road.get("car_count", 0) / road.get("capacity", 1), 1.0), 0.0)
-    return (c, 1 - c, 0)
+    c = road["workload"] / MAX_WORKLOAD
+    return (int(c * 255), int((1 - c) * 255), 0)
 
 
 def create_map_image(roads: list, size: tuple[int, int], colored=False) -> bytes:
@@ -26,7 +27,7 @@ def create_map_image(roads: list, size: tuple[int, int], colored=False) -> bytes
         ys.append(road["start"][1])
         ys.append(road["end"][1])
         if colored:
-            colors.append(get_road_color(road["car_count"], road["capacity"]))
+            colors.append(get_road_color(road))
     min_x, min_y = min(xs), min(ys)
     max_x, max_y = max(xs), max(ys)
     x_len = max_x - min_x
@@ -37,14 +38,14 @@ def create_map_image(roads: list, size: tuple[int, int], colored=False) -> bytes
         start, end = item[0]
         color = item[1] if colored else WHITE_COLOR
         new_start = (
-            (start[0] - min_x) / x_len * size[0] + MARGIN,
-            size[1] - (start[1] - min_y) / y_len * size[1] + MARGIN
+            int((start[0] - min_x) / x_len * size[0] + MARGIN),
+            int(size[1] - (start[1] - min_y) / y_len * size[1] + MARGIN)
         )
         new_end = (
-            (end[0] - min_x) / x_len * size[0] + MARGIN,
-            size[1] - (end[1] - min_y) / y_len * size[1] + MARGIN
+            int((end[0] - min_x) / x_len * size[0] + MARGIN),
+            int(size[1] - (end[1] - min_y) / y_len * size[1] + MARGIN)
         )
         draw.line((new_start, new_end), fill=color, width=LINE_SIZE)
     bs = BytesIO()
-    img.save(bs)
+    img.save(bs, format="png", bitmap_format="png")
     return bs.getvalue()
