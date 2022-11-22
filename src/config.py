408af9dfb8480d05a1281cdec285140ddf8d0e93
mirrin00 <mirrin1905@outlook.com>
2022-11-22 11:29:21 +0000

    Delted old files

--- src/config.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-TIMEOUT = datetime.timedelta(days=1)
-CURRENT_USERS_DB_NAME = "current_users"
-CURRENT_USERS_COLLECTION_NAME = "current_users"
-ORIGIN_DB_NAME = "origin_data"
-MAP_IMAGE_SIZE = (800, 800)  # in pixels
