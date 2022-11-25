a42dbf043559ce245858540bec8e88e5ee78b55e
mirrin00 <mirrin1905@outlook.com>
2022-11-25 20:09:36 +0000

    Added random ways

--- server/src/utils.py
+++ server/src/utils.py
@@ -1,6 +1,6 @@
 from pyproj import Transformer
 from pymongo.cursor import Cursor
-from random import randint
+from random import randint, choice
 import math
 import config
 
@@ -29,6 +29,10 @@ def transform_roads(roads: Cursor) -> list[dict]:
     return projected_roads, min_x, min_y, x_diff, y_diff
 
 
+def distance(point1: tuple, point2: tuple):
+    return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+
+
 def rotate_point(point: tuple[float, float], cos_value: float, sin_value: float, center: tuple[float, float] = (0.0, 0.0)):
     shifted_point = (
         point[0] - center[0],
@@ -54,7 +58,7 @@ def approximate_ellipse(p1: tuple[float, float], p2: tuple[float, float],
     angle = math.atan(abs(p1[1] - p2[1]) / max(abs(p1[0] - p2[0]), 0.000001))
     cos_value = math.cos(angle)
     sin_value = math.sin(angle)
-    x_len = math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) / 2
+    x_len = distance(p1, p2) / 2
     y_len = radius[0]
     quadrant1.append((
             center[0] + x_len,
@@ -77,10 +81,32 @@ def approximate_ellipse(p1: tuple[float, float], p2: tuple[float, float],
 
 
 def simulate(roads: Cursor, car_count: int) -> tuple[list[dict], list[dict]]:
-    new_roads = []
+    new_roads = {}
     for road in roads:
         cars = randint(0, road["capacity"])
         road["car_count"] = cars
         road["workload"] = round(cars / road["capacity"] * 10)
-        new_roads.append(road)
-    return new_roads, []
+        new_roads[(road["_id"])] = road
+    ways = []
+    roads_values = list(new_roads.values())
+    for _ in range(randint(0, 20)):
+        way = {
+            "time": 0.0,
+            "length": 0.0,
+            "points": []
+        }
+        cur_road = roads_values[randint(0, len(new_roads) - 1)]
+        way["points"].append(cur_road["location"][0])
+        for _ in range(len(new_roads) // 10):
+            way["points"].append(cur_road["location"][1])  # end
+            way["length"] += distance(cur_road["location"][0], cur_road["location"][0])
+            way["time"] += randint(10, 100) / 100
+            if len(cur_road["ways"]) == 0:
+                break
+            cur_road = new_roads.get(choice(cur_road["ways"]), cur_road)
+        ways.append(way)
+    return new_roads.values(), ways
+
+
+def scale_ways_points(points: list, min_x: float, min_y: float, x_coeff: float, y_coeff:float) -> list:
+    return [((p[0] - min_x) * x_coeff, (p[1] - min_y) * y_coeff) for p in points]
